f37073658493ce7536977dcabd289fb496eced2a
Ryan Taylor Long <ryan@rtlong.com>
2015-11-28 01:16:43 -0800

    Don't allow user keys less than 2048 bits

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,52 +13,69 @@ create_key() {
 		ssh-keygen -q -f "$file" -N '' "$@"
 	fi
 	if [[ ! -f ${file}.pub ]]; then
-		ssh-keygen -l -f "${file}.pub"
+		ssh-keygen -y -f "${file}" > "${file}.pub"
 	fi
 	if which restorecon >/dev/null 2>&1; then
 		restorecon "$file" "${file}.pub"
 	fi
+
+	# Print fingerprints out to log:
+	printf '\n### %s\n' "$file"
+	ssh-keygen -E sha256 -l -f "${file}.pub"
+	ssh-keygen -v -E md5 -l -f "${file}.pub"
 }
 
 create_keys() {
+	echo 'Generating SSH Host Keys...'
 	mkdir -p /etc/ssh/ssh_host_keys
-	create_key /etc/ssh/ssh_host_keys/rsa1_key -t rsa1
+	# create_key /etc/ssh/ssh_host_keys/rsa1_key -t rsa1
 	create_key /etc/ssh/ssh_host_keys/rsa_key -t rsa
 	create_key /etc/ssh/ssh_host_keys/dsa_key -t dsa
 	create_key /etc/ssh/ssh_host_keys/ecdsa_key -t ecdsa
 	create_key /etc/ssh/ssh_host_keys/ed25519_key -t ed25519
+	echo
+	echo
 }
 
 allow_github_user_via_ssh() {
 	local ghuser="$1"
 
 	echo "Adding user: ${ghuser}"
-	useradd \
-		--gid ssh-users \
-		--home-dir /tmp \
-		--no-user-group \
-		--no-create-home \
-		--shell /usr/sbin/nologin \
-		"${ghuser}"
 
-	mkdir -pv "/etc/ssh/per-user/${ghuser}/"
-	curl -fsSL "https://github.com/${ghuser}.keys" > "/etc/ssh/per-user/${ghuser}/authorized_keys"
+	mkdir -p "/etc/ssh/per-user/${ghuser}/"
 
 	echo 'Got the following keys:'
-	cat "/etc/ssh/per-user/${ghuser}/authorized_keys" | (
+	curl -fsSL "https://github.com/${ghuser}.keys" | (
 		local i=0
 		while read pubkey; do
-			echo $pubkey > "${ghuser}_key_${i}.pub"
-			ssh-keygen -E md5 -l -f "${ghuser}_key_${i}.pub"
-			rm -f "${ghuser}_key_${i}.pub"
+			local fn="${ghuser}_key_${i}.pub"
+			local fp=$(echo "$pubkey" > "$fn" && ssh-keygen -E md5 -l -f "$fn"; rm -f "$fn")
 			((i++)) || true
+
+			local keylength=$(echo $fp | awk '{ print $1 }')
+			if [[ $keylength -ge 2048 ]]; then
+				printf '  %s [less than 2048 - IGNORED]\n' "$fp"
+			else
+				echo "$pubkey" >> "/etc/ssh/per-user/${ghuser}/authorized_keys"
+				printf '  %s\n' "$fp"
+			fi
 		done
 	)
 
-	chown -c root:ssh-users "/etc/ssh/per-user/"
-	chown -Rc ${ghuser}:ssh-users "/etc/ssh/per-user/${ghuser}/"
-	chmod -Rc 0500 "/etc/ssh/per-user/${ghuser}/"
-	chmod -c 0400 "/etc/ssh/per-user/${ghuser}/authorized_keys"
+	# iif we found any valid keys via github, make the system user and fix perms
+	if [[ -f "/etc/ssh/per-user/${ghuser}/authorized_keys" ]]; then
+		useradd \
+			--gid ssh-users \
+			--home-dir /tmp \
+			--no-user-group \
+			--no-create-home \
+			--shell /usr/sbin/nologin \
+			"${ghuser}"
+		chown root:ssh-users "/etc/ssh/per-user/"
+		chown -R ${ghuser}:ssh-users "/etc/ssh/per-user/${ghuser}/"
+		chmod -R 0500 "/etc/ssh/per-user/${ghuser}/"
+		chmod 0400 "/etc/ssh/per-user/${ghuser}/authorized_keys"
+	fi
 }
 
 main() {
